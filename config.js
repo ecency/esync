7581494ecfe56332013ddf00257bc14d4b7e00b0
Feruz <fmiboy@gmail.com>
2018-03-17 07:40:06 +0200

    remote

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     options: {
-        url: "ws://localhost:8090",//"https://api.steemit.com",
+        url: "https://api.steemit.com",
         db_url: "mongodb://localhost:8882/steem_database",
         startingBlock: 19323600,
         delayBlocks: 21 //1 min
